5b3bb3271d1b95480e13231b7668904e4319cffe
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-07 12:10:08 +0300

    update colors, add gradient color for background

--- lib/common/ui/app_colors.dart
+++ lib/common/ui/app_colors.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryColor = Colors.black;
-  static const textPrimaryColor = Colors.black;
+  static const primaryColor = Colors.white;
+  static const textPrimaryColor = Colors.white;
+
+  static List<Color> backgroundGradient = [
+    Colors.grey[850]!,
+    Colors.grey[900]!,
+    Colors.black,
+  ];
 }
-
-
