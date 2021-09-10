0434203258423b9f8cfab7f1fce59d8975ed9591
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-10 13:59:32 +0300

    provide ErrorHandlerBloc in App

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,18 @@
+import 'package:crypto_info/common/bloc/error_handler_bloc/error_handler_bloc.dart';
+import 'package:crypto_info/common/bloc/error_handler_bloc/error_handler_state.dart';
 import 'package:crypto_info/common/env/config.dart';
 import 'package:crypto_info/common/env/debug_options.dart';
 import 'package:crypto_info/common/env/environment.dart';
+import 'package:crypto_info/common/error_listener.dart';
 import 'package:crypto_info/common/routes_factory.dart';
 import 'package:crypto_info/common/ui/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
+
+  ErrorListener get _errorListener => ErrorListener();
 
   DebugOptions get _debug => Environment<Config>.instance().config.debugOptions;
 
@@ -21,12 +27,20 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: _debug.debugShowCheckedModeBanner,
       initialRoute: RoutesFactory.initialRoute,
       onGenerateRoute: RoutesFactory().getGeneratedRoutes,
-      title: 'Template Bloc',
+      title: 'Crypto',
       theme: darkTheme,
       themeMode: ThemeMode.dark,
       builder: (context, child) => MediaQuery(
         data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
-        child: child!,
+        child: BlocProvider(
+          create: (_) => ErrorHandlerBloc(
+            Environment<Config>.instance().config.logger,
+          ),
+          child: BlocListener<ErrorHandlerBloc, ErrorHandlerState>(
+            listener: _errorListener.listener,
+            child: child!,
+          ),
+        ),
       ),
     );
   }
