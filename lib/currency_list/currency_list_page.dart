d6d4e1503a9681b061b74f4a335454c4865fac3a
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-13 17:04:04 +0300

    split currency list to widget

--- lib/currency_list/currency_list_page.dart
+++ lib/currency_list/currency_list_page.dart
@@ -1,71 +1,62 @@
-import 'package:crypto_info/common/ui/app_colors.dart';
 import 'package:crypto_info/common/ui/app_text_styles.dart';
 import 'package:crypto_info/common/ui/gradient_container.dart';
 import 'package:crypto_info/currency_list/bloc/currency_list_bloc.dart';
 import 'package:crypto_info/currency_list/bloc/currency_list_state.dart';
-import 'package:crypto_info/currency_list/model/currency_ui.dart';
+import 'package:crypto_info/currency_list/widget/currency_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CurrencyListPage extends StatelessWidget {
   const CurrencyListPage({Key? key}) : super(key: key);
 
-  Widget _buildCurrencyList() {
-    return BlocBuilder<CurrencyListBloc, CurrencyListState>(
-      builder: (context, state) {
-        return ListView.builder(
-          itemCount: state.currencies.length,
-          itemBuilder: (context, index) {
-            final currency = state.currencies[index];
+  Widget _buildTitleForCurrencyList() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              'Pair',
+              style: AppTextStyles.boldTextStyle.copyWith(fontSize: 20),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              child: Text(
+                'Volume',
+                style: AppTextStyles.boldTextStyle.copyWith(fontSize: 20),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Price',
+                style: AppTextStyles.boldTextStyle.copyWith(fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24,
-                vertical: 12,
+  Widget _buildCurrencyList() {
+    return SafeArea(
+      child: BlocBuilder<CurrencyListBloc, CurrencyListState>(
+        builder: (context, state) {
+          return Column(
+            children: [
+              _buildTitleForCurrencyList(),
+              Expanded(
+                child: CurrencyList(currencies: state.currencies),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Text(
-                      currency.name,
-                      style: AppTextStyles.boldTextStyle,
-                    ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      child: Text(
-                        currency.baseVolume,
-                        style: AppTextStyles.lightTextStyle,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          currency.price,
-                          style: AppTextStyles.mediumTextStyle,
-                        ),
-                        Text(
-                          currency.percentChange,
-                          style: AppTextStyles.regularTextStyle.copyWith(
-                            color:
-                                currency.priceState == CurrencyUiPriceState.rise
-                                    ? AppColors.priceRise
-                                    : AppColors.priceDrop,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        );
-      },
+            ],
+          );
+        },
+      ),
     );
   }
 
---
+++ lib/currency_list/widget/currency_list.dart
@@ -0,0 +1,66 @@
+import 'package:crypto_info/common/ui/app_colors.dart';
+import 'package:crypto_info/common/ui/app_text_styles.dart';
+import 'package:crypto_info/currency_list/model/currency_ui.dart';
+import 'package:flutter/material.dart';
+
+class CurrencyList extends StatelessWidget {
+  const CurrencyList({
+    required List<CurrencyUi> currencies,
+    Key? key,
+  })  : _currencies = currencies,
+        super(key: key);
+
+  final List<CurrencyUi> _currencies;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _currencies.length,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 12,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Text(
+                _currencies[index].name,
+                style: AppTextStyles.boldTextStyle,
+              ),
+            ),
+            Expanded(
+              child: Align(
+                child: Text(
+                  _currencies[index].baseVolume,
+                  style: AppTextStyles.lightTextStyle,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _currencies[index].price,
+                    style: AppTextStyles.mediumTextStyle,
+                  ),
+                  Text(
+                    _currencies[index].percentChange,
+                    style: AppTextStyles.regularTextStyle.copyWith(
+                      color: _currencies[index].priceState ==
+                              CurrencyUiPriceState.rise
+                          ? AppColors.priceRise
+                          : AppColors.priceDrop,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
