5020c22c087576e39d769fc3ab0525057891a095
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-13 15:24:15 +0300

    add CurrencyUi model

---
+++ lib/currency_list/model/currency_ui.dart
@@ -0,0 +1,52 @@
+import 'package:crypto_info/currency_list/model/currency_data_response.dart';
+import 'package:equatable/equatable.dart';
+
+enum CurrencyUiPriceState { drop, rise }
+
+class CurrencyUi extends Equatable {
+  const CurrencyUi({
+    required this.id,
+    required this.currencyPair,
+    required this.name,
+    required this.price,
+    required this.baseVolume,
+    required this.percentChange,
+    required this.priceState,
+  });
+
+  factory CurrencyUi.fromResponse({
+    required String currencyPair,
+    required CurrencyDataResponse response,
+  }) {
+    return CurrencyUi(
+      id: response.id,
+      currencyPair: currencyPair,
+      name: currencyPair.split('_').reversed.join('/'),
+      price: response.last.toStringAsFixed(8),
+      baseVolume: response.baseVolume.toStringAsFixed(0),
+      percentChange: (response.percentChange * 100).toStringAsFixed(2),
+      priceState: response.percentChange > 0
+          ? CurrencyUiPriceState.rise
+          : CurrencyUiPriceState.drop,
+    );
+  }
+
+  final int id;
+  final String currencyPair;
+  final String name;
+  final String price;
+  final String baseVolume;
+  final String percentChange;
+  final CurrencyUiPriceState priceState;
+
+  @override
+  List<Object?> get props => [
+        id,
+        currencyPair,
+        name,
+        price,
+        baseVolume,
+        percentChange,
+        priceState,
+      ];
+}
