30b5d2e1ac0b7b6ec7368ca2048abac520493a87
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-13 15:26:49 +0300

    add GetIt

--- lib/common/runner.dart
+++ lib/common/runner.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 
 import 'package:crypto_info/app.dart';
+import 'package:crypto_info/common/di/di.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<void> run() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await registerDependencies();
+  await getIt.allReady();
 
   Paint.enableDithering = true;
 
