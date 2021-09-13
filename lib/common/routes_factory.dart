be1d5b305a0adaddbeacd79f7af3189ca78626f9
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-13 15:27:13 +0300

    pass all dependencies to currencyList in RoutesFactory

--- lib/common/routes_factory.dart
+++ lib/common/routes_factory.dart
@@ -1,18 +1,28 @@
+import 'package:crypto_info/common/bloc/error_handler_bloc/error_handler_bloc.dart';
+import 'package:crypto_info/common/di/di.dart';
 import 'package:crypto_info/common/routes.dart';
+import 'package:crypto_info/currency_list/bloc/currency_list_bloc.dart';
+import 'package:crypto_info/currency_list/bloc/currency_list_event.dart';
+import 'package:crypto_info/currency_list/bloc/currency_list_repository.dart';
 import 'package:crypto_info/currency_list/currency_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RoutesFactory {
   static String get initialRoute => Routes.currencyList;
 
   Map<String, Widget Function(BuildContext)> get _routes => {
-        Routes.currencyList: (context) {
-          return const AnnotatedRegion<SystemUiOverlayStyle>(
-            value: SystemUiOverlayStyle.light,
-            child: CurrencyListPage(),
-          );
-        },
+        Routes.currencyList: (context) => BlocProvider(
+              create: (_) => CurrencyListBloc(
+                errorHandlerBloc: context.read<ErrorHandlerBloc>(),
+                currencyListRepository: getIt<CurrencyListRepository>(),
+              )..add(CurrencyListLoaded()),
+              child: const AnnotatedRegion<SystemUiOverlayStyle>(
+                value: SystemUiOverlayStyle.light,
+                child: CurrencyListPage(),
+              ),
+            ),
       };
 
   Route<dynamic> getGeneratedRoutes(RouteSettings settings) {
