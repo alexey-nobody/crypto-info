b5d1ab87d564b780b6bf896908e0328be666de3a
Alexey Lepskii <alexey.lepskii@gmail.com>
2021-09-14 13:49:34 +0300

    create widget CurrencyListRowTitles for CurrencyListPage

--- lib/currency_list/currency_list_page.dart
+++ lib/currency_list/currency_list_page.dart
@@ -1,65 +1,42 @@
-import 'package:crypto_info/common/ui/app_text_styles.dart';
 import 'package:crypto_info/common/ui/gradient_container.dart';
 import 'package:crypto_info/currency_list/bloc/currency_list_bloc.dart';
+import 'package:crypto_info/currency_list/bloc/currency_list_event.dart';
 import 'package:crypto_info/currency_list/bloc/currency_list_state.dart';
+import 'package:crypto_info/currency_list/model/currency_list_sorting_type.dart';
 import 'package:crypto_info/currency_list/widget/currency_list.dart';
+import 'package:crypto_info/currency_list/widget/currency_list_row_titles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CurrencyListPage extends StatelessWidget {
   const CurrencyListPage({Key? key}) : super(key: key);
 
-  Widget _buildDivider() {
-    return const Divider(
-      height: 20,
-      thickness: 1,
-      indent: 20,
-      endIndent: 20,
-    );
+  void _onChangeVolumeSorting(
+    BuildContext context,
+    CurrencyListSortingType sortingType,
+  ) {
+    context.read<CurrencyListBloc>().add(CurrencyListSorted(sortingType));
   }
 
-  Widget _buildTitleForCurrencyList() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Text(
-              'Pair',
-              style: AppTextStyles.boldTextStyle.copyWith(fontSize: 20),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              child: Text(
-                'Volume',
-                style: AppTextStyles.boldTextStyle.copyWith(fontSize: 20),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'Price',
-                style: AppTextStyles.boldTextStyle.copyWith(fontSize: 20),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCurrencyList() {
+  Widget _buildCurrencyList(BuildContext context) {
     return SafeArea(
       child: BlocBuilder<CurrencyListBloc, CurrencyListState>(
         builder: (context, state) {
           return Column(
             children: [
-              _buildTitleForCurrencyList(),
-              _buildDivider(),
+              CurrencyListRowTitles(
+                sortingType: state.sortingType,
+                onChangeSorting: (sortingType) => _onChangeVolumeSorting(
+                  context,
+                  sortingType,
+                ),
+              ),
+              const Divider(
+                height: 1,
+                thickness: 0.5,
+                indent: 20,
+                endIndent: 20,
+              ),
               Expanded(
                 child: CurrencyList(currencies: state.currencies),
               ),
@@ -76,7 +53,7 @@ class CurrencyListPage extends StatelessWidget {
       body: Stack(
         children: [
           const GradientContainer(),
-          _buildCurrencyList(),
+          _buildCurrencyList(context),
         ],
       ),
     );
