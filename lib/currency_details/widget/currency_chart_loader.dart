12049ae02e2a3e94186b75c6d754466dd905d223
Alexey Lepskii <alexey.lepskii@gmail.com>
2022-01-24 14:40:43 +0300

    add CurrencyChartLoader widget

---
+++ lib/currency_details/widget/currency_chart_loader.dart
@@ -0,0 +1,21 @@
+import 'package:crypto_info/common/ui/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+
+class CurrencyChartLoader extends StatelessWidget {
+  const CurrencyChartLoader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: AppColors.primary,
+      highlightColor: AppColors.shimmerHighlight,
+      child: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width,
+        height: 130.sp,
+      ),
+    );
+  }
+}
